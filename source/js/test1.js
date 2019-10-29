19eac1b02fb58020a0ead492b68cdd1baa551429
bunnyhouse <756214593@qq.com>
2019-10-30 00:37:29 +0800

    Update 2019-10-29T16:37:29.479Z

--- source/js/test1.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    function concat(a, b) {
-        return [...a, ...b];
-    }
-    // 用ES6扩展运算符实现合并数组操作
-    console.log(concat([1, 2, 3], [4, 5, 6]));
-})();
--- source/js/test2.js
+++
@@ -1,7 +0,0 @@
-(() => {
-    let newFun = () => {
-        console.log('lalala');
-    }
-    newFun();
-    console.log('nana');
-})();
